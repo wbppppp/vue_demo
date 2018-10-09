f79efd3679c161ab9a681a4ccc31b61eb26b459c
wbp <1871960139@qq.com>
2018-10-09 12:08:01 +0800

    vue基础用法

---
+++ src-base/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{msg}}</p>
+  </div>
+
+</template>
+
+<script>
+  //组件中，必须使用data函数来赋值
+  export default {
+    data () {
+      return {
+        msg: 'Hello Vue Component'
+      }
+    }
+  }
+</script>
+
+<style>
+  p{
+    color: red;
+    font-size: 40px;
+  }
+</style>

d56e2a7a8b76f9f4c0f0f000a9c681f705d21c19
wbp <1871960139@qq.com>
2018-10-10 17:00:06 +0800

    交互添加&删除&footer组件功能

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,62 @@
 <template>
-  <div>
-    <header class="site-header jumbotron">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12">
-            <h1>请发表对React的评论</h1>
-          </div>
-        </div>
-      </div>
-    </header>
-    <div class="container">
-      <!--将addComment方法传递给Add组件中-->
-      <Add :addComment="addComment"/>
-      <List :comments="comments" :deleteComment="deleteComment"/>
+  <div class="todo-container">
+    <div class="todo-wrap">
+
+      <TodoHeader :addItem="addItem"/>
+      <TodoList :todos="todos" :deleteItem="deleteItem"/>
+      <todo-footer :todos="todos" :deleteCompleteTodo="deleteCompleteTodo" :selectAllTodos="selectAllTodos"/>
+
     </div>
   </div>
 </template>
 
 <script>
-  import Add from './components/Add'
-  import List from './components/List'
-  export default {
-    components: {List, Add},
-    data() { //数据在哪个组件中，更新数据的方法就应该在哪个组件中
-      return {
-        comments:[
-          {name:'Bob',content:'Vue1'},{name:'ZZ',content:'Vue2'},{name:'BB',content:'Vue3'}
-        ]
-      }
-    },
-    methods:{
-      addComment(comment){
-        this.comments.unshift(comment);
+  import TodoHeader from './commponents/TodoHeader'
+  import TodoList from './commponents/TodoList'
+  import TodoFooter from './commponents/TodoFooter'
+    export default {
+      components: {
+        TodoHeader,
+        TodoList,
+        TodoFooter
       },
-      deleteComment(index){
-        this.comments.splice(index,1);
+      data(){
+        return {
+          todos:[
+            {title: '吃饭', complete: false},
+            {title: '睡觉', complete: true},
+            {title: '撸代码', complete: false}
+          ]
+        }
+      },
+      methods:{
+        deleteItem(index){
+          this.todos.splice(index,1);
+        },
+        addItem(item){
+          this.todos.unshift(item);
+        },
+        //删除选中的任务
+        deleteCompleteTodo(){
+          this.todos = this.todos.filter(todo => !todo.complete);
+        },
+        //全选/全不选
+        selectAllTodos(isAll){
+          this.todos.forEach(todo => todo.complete = isAll);
+        }
       }
     }
-  }
 </script>
 
 <style>
-  .reply {
-    margin-top: 0px;
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
   }
-
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import './base.css'
 
 new Vue({
   el: '#app',
